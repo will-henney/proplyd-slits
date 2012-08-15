eb258510193cccfcbe5b6e4e40655784fd24ad1f
Alejandro Tarango <j.tarango@crya.unam.mx>
2012-08-14 19:14:18 -0500

    Fundamental changes to this library, instead of reading the columns '_RAJ2000' and '_DEJ2000' from the fits file with the proplyd's coords (these columns give wrong coords), this time the program reads the colums with the coords in d m s for declination and h m s for RA. Also converts these coords to degrees

--- proplyds.py
+++ proplyds.py
@@ -1,6 +1,29 @@
 import pyfits
 import numpy as np
 
+def hms2deg(char):
+    """
+    Function used to transform the Right Ascension from the fits file given in hours, minutes and seconds
+    to degrees.
+    Argument: THe RA as a string of characters
+    output: A float number: the RA written in degrees
+    """
+    h,m,s = [float(x) for x in char.split()]
+    out = 15*(h+m/60.+s/3600.)
+    return out
+
+def dms2deg(char):
+    
+    """
+    Function used to transform the Declination from the fits file given in degrees, minutes and seconds
+    to degrees.
+    Argument: The dec as a string of characters
+    output: A float number: the Dec written in degrees
+    """
+    d,m,s = [float(x) for x in char.split()]
+    m,s = np.copysign(m,d),np.copysign(s,d) # The arcminutes and arcseconds adquire the same sign of degrees to be added later
+    out = (d + m/60.+ s/3600.) 
+    return out
 
 def ra_dec_prop(path,filename):
 
@@ -10,31 +33,11 @@ def ra_dec_prop(path,filename):
 
     ra_list = table.RA2000
     
-    ra_split = ra_list.split()
-
-    hour = [float(ra_split[x][0]) for x in np.arange(np.size(ra_list))]
-    minute = [float(ra_split[x][1]) for x in np.arange(np.size(ra_list))]
-    second = [float(ra_split[x][2]) for x in np.arange(np.size(ra_list))]
-
-    hr,mn,sec = np.array(hour), np.array(minute),np.array(second)
-     
-
-    ra = 15*(hr + mn/60. + sec/3600.) #In degrees
+    ra = np.array([hms2deg(z) for z in ra_list]) 
 
     dec_list = table.DE2000
-
-    dec_split = dec_list.split() 
-
-    degree = [float(dec_split[x][0]) for x in np.arange(np.size(dec_list))]
-    arcminute = [float(dec_split[x][1]) for x in np.arange(np.size(dec_list))]
-    arcsecond = [float(dec_split[x][2]) for x in np.arange(np.size(dec_list))]
-
-    deg,amin,asec = np.array(degree), np.array(arcminute),np.array(arcsecond)
-
-    amin = np.copysign(amin,deg)  # The arcminutes and arcseconds adquire the same sign of degrees to be added later
-    asec = np.copysign(asec,deg)
     
-    dec = (deg + amin/60. + asec/3600.) #In degrees     
+    dec = np.array([dms2deg(z) for z in dec_list])   
 
     names = table.__OW94_
     
